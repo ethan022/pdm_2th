942cbf077dd3a2fce1128cd6f3a5127251ef0b85
Leader_Ethan <ethanyun022@gmail.com>
2026-07-24 13:53:39 +0900

    수업 실습 파일 정리
    - 0724/02.py, 03.py 삭제 (내용은 복습 자료에 반영됨)
    - 0724/01.py 수업 중 작성 코드 갱신

--- 0724/01.py
+++
@@ -1,5 +0,0 @@
-text = "정 상 가 동"
-
-text = text.replace(" ", "")
-
-print(text)  # 정상가동
--- 0724/02.py
+++
@@ -1,6 +0,0 @@
-fname = "Sensor_LOG.CSV"
-low_fname = fname.lower()
-
-print(low_fname.startswith("sensor"))
-print(low_fname.endswith(".csv"))
-print(fname.endswith(".csv"))
